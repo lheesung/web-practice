b23622d45267cf1cbf42de9e38b33111efe76151
HeeSungLee <a73482006@gmail.com>
2022-11-12 23:46:23 +0900

    ADD::header-style

--- clone-coding/astonmartin/src/components/Header.js
+++ clone-coding/astonmartin/src/components/Header.js
@@ -1,15 +1,22 @@
 import logo from '../images/logo.svg';
 import './Header.css';
+import background1 from '../images/background1.jpeg';
 function Header(){
     return(
         <>
+            <img src = {background1} className="background"/>
             <header className='header'>
-                <div className='con1'>
-                    <p>브랜드 소개</p>
-                    <p>모델</p>
-                    <p>서비스</p>
+                <div className='cont'>
+                    <a href="#">브랜드소개</a>
+                    <a href="#">모델</a> 
+                    <a href="#">서비스</a>
                 </div>
                 <img src={logo} width='150px' height='70px'></img>
+                <div className='cont'>
+                    <a href="#">Q 바이 애스턴마틴 </a>
+                    <a href="#">뉴스</a>
+                    <a href="#">문의</a>
+                </div>
             </header>
         </>
     )

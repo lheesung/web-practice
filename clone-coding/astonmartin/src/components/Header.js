9d0f2500c6b33f70f7fbd656f342ad9afe4542be
HeeSungLee <a73482006@gmail.com>
2022-11-14 12:26:52 +0900

    ADD::header-function

--- clone-coding/astonmartin/src/components/Header.js
+++ clone-coding/astonmartin/src/components/Header.js
@@ -1,13 +1,28 @@
 import logo from '../images/logo.svg';
 import './Header.css';
 import background1 from '../images/background1.jpeg';
+import { useEffect, useRef } from 'react';
+
+
 function Header(){
+    const headerRef = useRef();
+    
+    useEffect(() => {
+        window.addEventListener('scroll', e=>{
+            if(window.scrollY >= 1){
+                headerRef.current.classList.add('opacity');
+            }
+            else{
+                headerRef.current.classList.remove('opacity');
+            }
+        });
+    }, []);
     return(
         <>
             <img src = {background1} className="background"/>
-            <header className='header'>
+            <header className='header' ref={headerRef}>
                 <div className='cont'>
-                    <a href="#">브랜드소개</a>
+                    <a href="#">브랜드 소개</a>
                     <a href="#">모델</a> 
                     <a href="#">서비스</a>
                 </div>

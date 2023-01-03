47babcfbc18ca74504b874a0ad6067c3ddfe878e
HeeSungLee <102288399+lheesung@users.noreply.github.com>
2023-01-03 19:47:09 +0900

    220103

--- web/React/learn/src/App.js
+++ web/React/learn/src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
 import Main from "./page/Main.js";
+import "./App.css"
 function App() {
+  const [randnum,setRand] = useState(0);
+  
+  function randomNumberInRange(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min;}
+  
+  const randbtn = () => {
+    setRand(randomNumberInRange (1,100));
+    // console.log(randnum);
+  }
+
   return (
     <>
-    <Main/>
+    <button onClick={randbtn}>get random number</button>
+    <Main random = {randnum}/>
     </>
   );
 }
--- web/React/learn/src/page/Main.js
+++ web/React/learn/src/page/Main.js
@@ -1,23 +1,37 @@
-import { useState, useEffect } from "react"
-import "./MainStyle.css"
-export default function Main(){
+import { useState, useEffect, useReducer } from "react";
+import "./MainStyle.css";
 
+export default function Main(props) {
+  const [name, setName] = useState("Jim");
+  const [propsValue, setProps] = useState(props.random); // props 로 받아온 값 바꾸려면 state 하나 만들어야함.
+  
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
 
-    const [name, setName] = useState("Jim");
+  console.log(props);
 
-    useEffect(()=> {
-        alert("qkqh")
-    }, [/*바뀌는 값 */]);
-
-    return(
-        <>
-        <div className="container">
-            <h1>useState</h1>
-            <h1 id='name'>{ name }</h1>
-            <button onClick={()=>{setName(name === "Jim" ? "Kevin" : "Jim")}}>Change State</button>
-            <br></br>
-            <h1>useEffect</h1>
+  return (
+    <>
+      <div className="container">
+        <div id="box">
+          <h1>useState</h1>
+          <h1 id="name">{name}</h1>
+          <button id="btn" onClick={() => {
+            setName(name === "Jim" ? "Kevin" : "Jim");
+            }}>Change State</button>
         </div>
-        </>
-    )
+
+        <div id="box">
+            <h1>useEffect</h1>
+            <h2>refer console</h2>
+        </div>
+
+        <div id="box">
+            <h1>props</h1>
+            <h2>Value : {props.random}</h2>
+        </div>
+      </div>
+    </>
+  );
 }
